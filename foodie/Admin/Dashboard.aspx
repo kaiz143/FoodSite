e66ab0bdfa907a5a3fe8110f4c4449220348c780
Altamash <altamashshaikh915631@gmail.com>
2024-12-22 16:00:56 +0530

    Add project files.

---
+++ foodie/Admin/Dashboard.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="foodie.Admin.Dashboard" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <h1> DashBoard Page</h1>
+</asp:Content>
---
+++ foodie/User/User.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace foodie.User
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+                //Load the Control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl1.ascx");
+
+                //Add the Control to the panel
+                PnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}
