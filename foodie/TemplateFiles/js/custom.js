f91ea0a99eae20a66a5ac4028459cbc9e45b3031
kaiz143 <fai44516@gmail.com>
2025-03-29 23:59:32 +0530

    Cart More than half done

--- foodie/TemplateFiles/js/custom.js
+++ foodie/TemplateFiles/js/custom.js
@@ -156,3 +156,33 @@ $(".client_owl-carousel").owlCarousel({
         }
     }
 });
+
+
+(function ($) {
+    /*-------------------------
+        Quantity change
+    -------------------------*/
+    var proQty = $('.pro-qty');
+    proQty.append('<span class="dec qtybtn">-</span>');
+    proQty.append('<span class="inc qtybtn">+</span>');
+    proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            //var newVal = parseFloat(oldValue) + 1;
+            if (oldValue >= 10) {
+                var newVal = parseFloat(oldValue);
+            } else {
+                newVal = parseFloat(oldValue) + 1;
+            }
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 1) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 1;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
+})(jQuery);
--- foodie/User/Cart.aspx.cs
+++ foodie/User/Cart.aspx.cs
@@ -1,17 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using foodie.Admin;
 
 namespace foodie.User
 {
     public partial class Cart : System.Web.UI.Page
     {
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+        decimal grandTotal = 0;
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserId"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                getCartItems();
+
+            }
+        }
+
+        void getCartItems()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Cart_Crud", con);
+
+            cmd.Parameters.AddWithValue("@Action", "SELECT");
+            cmd.Parameters.AddWithValue("@UserId", Session["UserId"]);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                // Bind data to Repeater and show it
+                rCartItem.DataSource = dt;
+                rCartItem.DataBind();
+
+                rCartItem.Visible = true;
+                //lblEmptyCart.Visible = false; // Hide empty message
+
+                // Calculate grand total and store it in session
+                grandTotal = dt.AsEnumerable().Sum(row => Convert.ToDecimal(row["Price"]) * Convert.ToInt32(row["Qty"]));
+                Session["grandTotalPrice"] = grandTotal;
+            }
+            else
+            {
+                // Show empty cart message
+                rCartItem.Visible = false;
+                //lblEmptyCart.Visible = true;
+                Session["grandTotalPrice"] = 0;
+            }
+        }
+
+        protected void rCartItem_ItemCommand(object source, RepeaterCommandEventArgs e)
         {
 
         }
+
+        protected void rCartItem_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Label totalPrice = e.Item.FindControl("lblTotalPrice") as Label;
+                Label productPrice = e.Item.FindControl("lblPrice") as Label;
+                TextBox quantity = e.Item.FindControl("txtQuantity") as TextBox;
+                decimal calTotalPrice = Convert.ToDecimal(productPrice.Text) * Convert.ToDecimal(quantity.Text);
+                totalPrice.Text = calTotalPrice.ToString();
+                grandTotal += calTotalPrice;
+            }
+            Session["grandTotalPrice"] = grandTotal;
+
+        }
+
+        // REMOVE this part
+        private sealed class CustomTemplate : ITemplate
+        {
+            private ListItemType ListItemType { get; set; }
+
+            public CustomTemplate(ListItemType type)
+            {
+                ListItemType = type;
+            }
+
+            public void InstantiateIn(Control container)
+            {
+                if (ListItemType == ListItemType.Footer)
+                {
+                    var footer = new LiteralControl("<tr><td colspan='5'><b>Your Cart is empty.</b><a href='Menu.aspx' class='badge badge-info ml-2'></a></td></tr></tbody></table>");
+                    container.Controls.Add(footer);
+                }
+            }
+        }
+
+
     }
 }
